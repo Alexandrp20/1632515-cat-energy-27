6f50e2545457bf46566515036c566ba8f31fddb6
Alexander <webfront22@gmail.com>
2024-01-23 23:06:35 +0700

    b

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import pug from 'gulp-pug';
 
 // Styles
 
@@ -17,6 +18,19 @@ export const styles = () => {
     .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
+
+
+
+// const { src, dest } = require('gulp');
+// const pug = require('gulp-pug');
+
+export const pug = () => {
+  return gulp.src('source/pug/pages/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('source'));
+};
 
 // Server
 
